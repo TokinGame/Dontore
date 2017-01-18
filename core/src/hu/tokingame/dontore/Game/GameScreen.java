a8fc805df311b829999a276fc1215eea26b3fd6c
davimatyi <davimatyi@outlook.com>
2017-01-18 17:38:48 +0100

    stage-ek meg screen megvan csak működőképesre kell csinálni meg van doboz meg tüske textúra majd lehet cserélni ha kell

---
+++ core/src/hu/tokingame/dontore/Game/GameScreen.java
@@ -0,0 +1,39 @@
+package hu.tokingame.dontore.Game;
+
+import hu.tokingame.dontore.MyBaseClasses.MyScreen;
+import hu.tokingame.dontore.MyGdxGame;
+
+/**
+ * Created by davimatyi on 2017. 01. 18..
+ */
+
+public class GameScreen extends MyScreen {
+    public GameScreen(MyGdxGame game) {
+        super(game);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+    }
+
+    @Override
+    public void init() {
+        super.init();
+    }
+
+    @Override
+    public void resume() {
+        super.resume();
+    }
+}
---
+++ core/src/hu/tokingame/dontore/Game/HostedGameStage.java
@@ -0,0 +1,32 @@
+package hu.tokingame.dontore.Game;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import hu.tokingame.dontore.MyBaseClasses.MyStage;
+import hu.tokingame.dontore.MyGdxGame;
+
+/**
+ * Created by davimatyi on 2017. 01. 18..
+ */
+
+public class HostedGameStage extends MyStage {
+    public HostedGameStage(Viewport viewport, Batch batch, MyGdxGame game) {
+        super(viewport, batch, game);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
