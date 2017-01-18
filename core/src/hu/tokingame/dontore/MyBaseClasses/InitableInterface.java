699c19e3e05cf3b3d95cf25f771a16f8ac86956f
Marsu5 <marcell.schuh@gmail.com>
2017-01-18 13:09:54 +0100

    Sajt

--- core/src/hu/tokingame/dontore/MyBaseClasses/InitableInterface.java
+++
@@ -1,9 +0,0 @@
-package hu.tokingame.dontore.MyBaseClasses;
-
-/**
- * Created by davimatyi on 2017. 01. 10..
- */
-
-public interface InitableInterface {
-    public void init();
-}
--- core/src/hu/tokingame/dontore/MyBaseClasses/ShapeType.java
+++
@@ -1,8 +0,0 @@
-package hu.tokingame.dontore.MyBaseClasses;
-
-/**
- * Created by davimatyi on 2017. 01. 10..
- */
-public enum ShapeType {
-    Circle, Rectangle, Polygon
-}
