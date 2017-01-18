408302dc3d5d0a316fb38c912eb15db3a6d31345
davimatyi <davimatyi@outlook.com>
2017-01-18 18:56:37 +0100

    dik világ

--- core/src/hu/tokingame/dontore/Game/GameScreen.java
+++ core/src/hu/tokingame/dontore/Game/GameScreen.java
@@ -1,5 +1,12 @@
 package hu.tokingame.dontore.Game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.utils.shapebuilders.ConeShapeBuilder;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+
+import hu.tokingame.dontore.Global.Globals;
 import hu.tokingame.dontore.MyBaseClasses.MyScreen;
 import hu.tokingame.dontore.MyGdxGame;
 
@@ -8,8 +15,17 @@ import hu.tokingame.dontore.MyGdxGame;
  */
 
 public class GameScreen extends MyScreen {
+
+    SinglePlayerStage stage;
+
+
     public GameScreen(MyGdxGame game) {
         super(game);
+        stage = new SinglePlayerStage(new ExtendViewport(16, 16, Globals.WORLD_WIDTH, Globals.WORLD_HEIGHT), new SpriteBatch(), game);
+
+
+
+
     }
 
     @Override
@@ -25,6 +41,7 @@ public class GameScreen extends MyScreen {
     @Override
     public void render(float delta) {
         super.render(delta);
+        stage.act(delta);
     }
 
     @Override
--- core/src/hu/tokingame/dontore/Game/SinglePlayerStage.java
+++ core/src/hu/tokingame/dontore/Game/SinglePlayerStage.java
@@ -1,9 +1,18 @@
 package hu.tokingame.dontore.Game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
+import hu.tokingame.dontore.Global.Globals;
 import hu.tokingame.dontore.MyBaseClasses.MyStage;
+import hu.tokingame.dontore.MyBaseClasses.WorldBodyEditorLoader;
 import hu.tokingame.dontore.MyGdxGame;
 
 /**
@@ -11,8 +20,28 @@ import hu.tokingame.dontore.MyGdxGame;
  */
 
 public class SinglePlayerStage extends MyStage {
+
+    World world;
+    Box2DDebugRenderer box2DDebugRenderer;
+    WorldBodyEditorLoader loader;
+    ControlStage controlStage;
+    PauseStage pauseStage;
+
     public SinglePlayerStage(Viewport viewport, Batch batch, MyGdxGame game) {
         super(viewport, batch, game);
+
+        world = new World(new Vector2(1, 0), false);
+        box2DDebugRenderer = new Box2DDebugRenderer();
+        loader = new WorldBodyEditorLoader(Gdx.files.internal("phys.json"));
+        controlStage = new ControlStage(new ExtendViewport(16, 16, Globals.WORLD_WIDTH, Globals.WORLD_HEIGHT), new SpriteBatch(), game);
+        pauseStage = new PauseStage(new ExtendViewport(16, 16, Globals.WORLD_WIDTH, Globals.WORLD_HEIGHT), new SpriteBatch(), game);
+
+        InputMultiplexer inputMultiplexer = new InputMultiplexer();
+        inputMultiplexer.addProcessor(this);
+        inputMultiplexer.addProcessor(controlStage);
+        inputMultiplexer.addProcessor(pauseStage);
+        Gdx.input.setInputProcessor(inputMultiplexer);
+
     }
 
     @Override
